466d7f356932c50ccba1a14d292c19dd293750c2
Hoang Quan <quanhv@runsystem.net>
2017-05-25 09:48:39 +0700

    Finish static page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @base_title = 'My app is awesome applicction'
+  end
   test "should get home" do
     get 'http://localhost:3000/static_pages/home'
     assert_response :success # :not_found, false
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test 'show get about' do
     get static_pages_about_url
 
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
