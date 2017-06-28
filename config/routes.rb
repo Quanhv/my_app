f11f256195a8964549f6d31270927e7419013886
Hoang Quan <quanhv@runsystem.net>
2017-06-28 09:08:07 +0700

    add test for signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/about', as: 'about'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,8 +61,8 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.email
   end
 
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+  test "password should be present" do
+    @user.password = @user.password_confirmation = ""
     assert_not @user.valid?
   end
   test "password should have a minimum length" do
