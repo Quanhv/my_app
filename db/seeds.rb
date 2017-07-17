ad160670ad8931690a87fd8eea978200619bfa66
Hoang Quan <quanhv@runsystem.net>
2017-07-17 10:32:40 +0700

    Finished User CRUDs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.count == 0
+  100.times do |i|
+    User.create(name: "Name #{i+1}", email: "quan#{i}@gmail.com", password: '123456')
+  end
+end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,36 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:user1)
+    @admin = users(:admin)
+    @other_user = users(:user2)
+  end
   test "should get new" do
     get signup_url
     assert_response :success
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    login_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should delete user and redirect to users index" do
+    login_as(@admin)
+    assert_difference 'User.count', -1 do
+      delete user_path(@user)
+    end
+    assert_redirected_to users_url
+  end
 end
