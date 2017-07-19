a85a3553586cd6f285c283e159cc2492ec1d3b7b
Hoang Quan <quanhv@runsystem.net>
2017-07-19 10:46:16 +0700

    reset password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717030852) do
+ActiveRecord::Schema.define(version: 20170718121456) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170717030852) do
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
